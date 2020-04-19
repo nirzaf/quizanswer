31d07b0335966ccb4ba161f2830cff18538a4c92
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-19 19:53:21 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < i+1; i++)
+            {
+                if (i % 2 == 1 && i % 3 == 1 && i % 4 == 1 && i % 5 == 1 && i % 6 == 1 && i % 7 == 0)
+                {
+                    Console.WriteLine(i.ToString());
+                    break;
+                }
+            }
+        }
+    }
+}
